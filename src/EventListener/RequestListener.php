dca17bea1490a21a20d8b736556e2344cbd44036
Gustaw Rydzak <gustaw.rydzak@gmail.com>
2024-07-03 11:45:23 +0200

    FEAT Add x-task: 1 to every response in application

---
+++ src/EventListener/RequestListener.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+final class RequestListener
+{
+    #[AsEventListener(event: KernelEvents::RESPONSE)]
+    public function onKernelResponse(ResponseEvent $event): void
+    {
+        if (!$event->isMainRequest()) {
+            return;
+        }
+
+        $responseHeaders = $event->getResponse()->headers;
+        $responseHeaders->set('x-task', 1);
+    }
+}
