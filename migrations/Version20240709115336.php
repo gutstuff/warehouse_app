b35abb27687ca96d7b0587bcae3168a9accfbdf5
Gustaw Rydzak <gustaw.rydzak@gmail.com>
2024-07-09 14:21:03 +0200

    FIX Add migration name

--- migrations/Version20240709115336.php
+++ migrations/Version20240709115336.php
@@ -14,7 +14,7 @@ final class Version20240709115336 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Initial migration';
     }
 
     public function up(Schema $schema): void
